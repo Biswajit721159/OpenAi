87b13a3d991212e84646f6d5f645c766e7728704
Biswajit721159 <bg5050525@gmail.com>
2024-02-04 12:26:15 +0530

    adding the seraching bar

--- riktamai/src/component/Main.js
+++ riktamai/src/component/Main.js
@@ -3,11 +3,11 @@ import { IoSend } from "react-icons/io5";
 import { BeatLoader } from 'react-spinners'
 import ShowData from "./ShowData";
 import { sendtoAi } from './APICall'
-// let api = process.env.REACT_APP_API_URL
 const Main = () => {
 
   const msgEnd = useRef(null)
   const [loader, setloader] = useState(false)
+
   let [message, setMessage] = useState([
     {
       "role": 'assistant',
@@ -20,16 +20,6 @@ const Main = () => {
   }, [message])
 
   let [searching_description, setsearching_description] = useState('')
-
-  function getPreviousState(message) {
-    let ans = []
-    for (let i = 1; i < message.length; i++) {
-      if (message[i].role == "user") {
-        ans.push(message[i].content)
-      }
-    }
-    return ans;
-  }
 
   async function sendToChatGpt() {
 
@@ -44,61 +34,10 @@ const Main = () => {
     message.push(newMessage)
     setMessage([...message]);
 
-    let data = await sendtoAi(message,text)
+    let data = await sendtoAi(message, text)
     setMessage([...data.data])
     setloader(data.isload)
-
-
-    // if (searching_description.length === 0) return
-    // let text = searching_description;
-    // let previoustext=getPreviousState(message)
-    // previoustext.push(text)
-    // // console.log(previoustext)
-    // setloader(true)
-    // setsearching_description('')
-    // let newMessage = {
-    //   "role": 'user',
-    //   "content": text
-    // };
-    // message.push(newMessage)
-    // setMessage([...message]);
-    // fetch(`${api}`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     searching_description: previoustext
-    //   })
-    // }).then((responce) => responce.json())
-    //   .then((res) => {
-    //     console.log(res)
-    //     setloader(false)
-    //     if (res.statusCode === 200) {
-    //       let data = {
-    //         'role': 'assistant',
-    //         'content': res.data
-    //       }
-    //       setMessage([...message, data]);
-    //     }
-    //     else if (res.statusCode === 500) {
-    //       let data = {
-    //         'role': 'assistant',
-    //         'content': res.data
-    //       }
-    //       setMessage([...message, data]);
-    //     }
-    //   }).catch((error) => {
-    //     setloader(false)
-    //     let data = {
-    //       'role': 'assistant',
-    //       'content': "Sorry, we are not able to answer your question."
-    //     }
-    //     setMessage([...message, data]);
-    //   })
   }
-
 
   const handelEnter = async (e) => {
     if (e.key === 'Enter') {
@@ -106,9 +45,10 @@ const Main = () => {
     }
   }
 
-  const [rows, setrows] = useState(1)
-  const setsearchvalue = (value) => {
-    setsearching_description(value)
+  const setsearchvalue = (event) => {
+    setsearching_description(event.target.value)
+    event.target.style.height = 'auto';
+    event.target.style.height = event.target.scrollHeight + 'px';
   }
 
 
@@ -123,8 +63,8 @@ const Main = () => {
       </div>
       <div className='chatFooter'>
         <div className='inp'>
-          <textarea type='text' rows={rows} disabled={loader} value={searching_description} onKeyDown={handelEnter} onChange={(e) => setsearchvalue(e.target.value)} placeholder="Send a message" className="inputfrom" />
-          <button disabled={loader} className='send' onClick={sendToChatGpt}><IoSend size={'30px'} /></button>
+          <textarea type='text' rows={1} disabled={loader} value={searching_description} onKeyDown={handelEnter} onChange={(e) => setsearchvalue(e)} placeholder="Send a message" className="inputfrom" />
+          <button disabled={loader} className='send' onClick={sendToChatGpt}><IoSend size={'25px'} /></button>
         </div>
       </div>
     </>
